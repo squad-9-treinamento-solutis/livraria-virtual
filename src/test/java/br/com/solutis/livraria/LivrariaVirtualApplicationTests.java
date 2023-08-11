2eb1d0b52951b6eba1a843da72ee6dc71d7ad522
Victor Kayk <victorkayk77@gmail.com>
2023-08-11 12:05:13 -0300

    chore: configurando o spring boot no projeto

---
+++ src/test/java/br/com/solutis/livraria/LivrariaVirtualApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.solutis.livraria;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LivrariaVirtualApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
