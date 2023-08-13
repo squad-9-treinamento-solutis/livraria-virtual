114bdc8c7b15f3cef9a027c35402011e0b11cc6c
ricardovac <ricardoviicius@gmail.com>
2023-08-13 03:12:02 -0300

    feat(book): removendo task deletar livro

--- src/main/java/br/com/solutis/livraria/controller/BookController.java
+++ src/main/java/br/com/solutis/livraria/controller/BookController.java
@@ -1,6 +1,5 @@
 package br.com.solutis.livraria.controller;
 
-import br.com.solutis.livraria.domain.Book;
 import br.com.solutis.livraria.domain.EBook;
 import br.com.solutis.livraria.domain.PrintedBook;
 import br.com.solutis.livraria.service.BookService;
@@ -17,7 +16,6 @@ import org.springframework.web.bind.annotation.*;
 public class BookController {
     private final BookService<EBook> eBookService;
     private final BookService<PrintedBook> printedBookService;
-    private final BookService<Book> bookService;
 
     @PostMapping(path = "/printed")
     public ResponseEntity<PrintedBook> addPrintedBook(@RequestBody @Valid PrintedBook printedBook) {
@@ -27,11 +25,5 @@ public class BookController {
     @PostMapping(path = "/eletronic")
     public ResponseEntity<EBook> addEbook(@RequestBody @Valid EBook eBook) {
         return new ResponseEntity<>(eBookService.addBook(eBook), HttpStatus.CREATED);
-    }
-
-    @DeleteMapping("/{id}")
-    public ResponseEntity<Void> remove(@PathVariable Long id) {
-        bookService.removeBook(id);
-        return new ResponseEntity<>(HttpStatus.NO_CONTENT);
     }
 }
--- src/main/java/br/com/solutis/livraria/service/BookService.java
+++ src/main/java/br/com/solutis/livraria/service/BookService.java
@@ -5,8 +5,6 @@ import br.com.solutis.livraria.repository.BookRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
-import java.util.Optional;
-
 @Service
 @RequiredArgsConstructor
 /*
@@ -17,10 +15,5 @@ public class BookService<T extends Book> {
 
     public T addBook(T book) {
         return bookRepository.save(book);
-    }
-
-    public void removeBook(Long id) {
-        Optional<T> optionalBook = bookRepository.findById(id);
-        optionalBook.ifPresent(bookRepository::delete);
     }
 }
